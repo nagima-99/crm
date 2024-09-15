9443be939278812a30722a02c1740fff76a0b6fb
nagima <nagimaazdarova@gmail.com>
2024-09-16 01:42:15 +0600

    changes

--- models.py
+++ models.py
@@ -1,6 +1,13 @@
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask import Flask
 
-db = SQLAlchemy()
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = '00cb48ff0bb190c58724e6b3834ced90'
+
+db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -8,7 +15,8 @@ class User(db.Model):
     password = db.Column(db.String(120), nullable=False)
     role = db.Column(db.String(20), nullable=False)
 
-    def __init__(self, username, password, role):
-        self.username = username
-        self.password = password
-        self.role = role
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
