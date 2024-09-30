af71797d2644f928ed00137bd7996d9ab9ed902b
Nagima <nagimaazdarova@gmail.com>
2024-09-30 15:13:59 +0500

    fix

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=c:\Users\506_3\Desktop\crm system\crm\.venv
+set VIRTUAL_ENV=c:\Users\2022\Desktop\crm\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
